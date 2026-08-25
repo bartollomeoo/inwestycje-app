d44d09a15d43fac594a0306115c86c06f212a5e0
Bartek <bartekwitek76@gmail.com>
2026-08-25 20:00:04 +0200

    po migracji

--- inwestycje_app/config.py
+++ inwestycje_app/config.py
@@ -5,5 +5,4 @@ APP_TITLE = "Aplikacja wspomagająca inwestycje giełdowe"
 PRICE_CACHE_TTL = 1800
 LATEST_PRICE_CACHE_TTL = 300
 
-# Lokalne SQLite pozostaje wyłącznie do jednorazowej migracji.
-LEGACY_DB_PATH = Path(os.getenv("LEGACY_DB_PATH", "baza.db"))
+
